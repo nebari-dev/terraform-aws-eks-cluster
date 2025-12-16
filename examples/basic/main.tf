ee412639a2e54ed343c34042f5dba0bebd2ca99b
Marcelo Villa <36754005+marcelovilla@users.noreply.github.com>
2025-12-16 15:31:57 +0100

    Simplify basic example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,22 +1,14 @@
-# This example creates a simple EKS cluster with minimal configuration
-
 module "eks" {
   source = "../.."
 
-  project_name = "nebari-aws-basic"
+  project_name = "eks-basic"
 
-  # Basic node group configuration
   node_groups = {
     general = {
       instance  = "m5.xlarge"
       min_nodes = 1
       max_nodes = 3
     }
-  }
-
-  tags = {
-    Environment = "development"
-    Project     = "nebari"
   }
 }
 
