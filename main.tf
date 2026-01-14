52f8d248e265ec6503ed36dfaaa6ed01e16d6b10
Marcelo Villa <36754005+marcelovilla@users.noreply.github.com>
2026-01-14 10:56:22 +0100

    Run tofu fmt

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ module "vpc" {
 
   private_subnet_tags = { "kubernetes.io/role/internal-elb" = "1" }
   public_subnet_tags  = { "kubernetes.io/role/elb" = "1" }
-  
+
   enable_nat_gateway     = true
   single_nat_gateway     = false
   one_nat_gateway_per_az = true
